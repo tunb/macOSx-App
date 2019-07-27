4e1178c155869be1203ac6ee1cbb8a3eadd667eb
Ajai Nair <ajai@ajais-mac.local>
2019-07-27 02:50:07 -0700

    Initial Commit

---
+++ EUR/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EUR
+//
+//  Created by Ajai Nair on 7/27/19.
+//  Copyright © 2019 mrb. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EUR/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EUR
+//
+//  Created by Ajai Nair on 7/27/19.
+//  Copyright © 2019 mrb. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
